9cff5c30b52fdd537d6b580699e3660cca877e70
Matt Toensing <matthew.toensing@gmail.com>
2021-09-03 15:29:10 -0700

    Create tes for merchant show route for API

--- spec/requests/api/v1/merchants_request_spec.rb
+++ spec/requests/api/v1/merchants_request_spec.rb
@@ -20,4 +20,17 @@ RSpec.describe 'merchants api' do
       expect(merchant[:attributes][:name]).to be_a(String)
     end
   end
+
+  it 'can get one merchant by its id' do
+    id = create(:merchant).id
+
+    get "/api/v1/merchants/#{id}"
+
+    merchant = JSON.parse(response.body, symbolize_names: true)[:data]
+
+    expect(response).to be_successful
+    expect(merchant).to have_key(:id)
+    expect(merchant[:id].to_i).to eq(id)
+    expect(merchant[:attributes]).to have_key(:name)
+  end
 end
