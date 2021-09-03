f947eb8571d128c7ac0b808aabf29dad3f559b8c
Matt Toensing <matthew.toensing@gmail.com>
2021-09-03 15:30:05 -0700

    Add show action for finding a merchant by id

--- app/controllers/api/v1/merchants_controller.rb
+++ app/controllers/api/v1/merchants_controller.rb
@@ -3,4 +3,8 @@ class Api::V1::MerchantsController < ApplicationController
     merchants = Merchant.paginate(:page => params[:page], :per_page => 20)
     render(json: MerchantSerializer.new(merchants))
   end
+
+  def show
+    render(json: MerchantSerializer.new(Merchant.find(params[:id])))
+  end
 end
