72146695af76724d1fd9e213d90b1b3b478eb527
Matt Toensing <matthew.toensing@gmail.com>
2021-09-06 16:00:36 -0700

    Add sad path test for no record of merchant id in system

--- spec/requests/api/v1/merchant_items_request_spec.rb
+++ spec/requests/api/v1/merchant_items_request_spec.rb
@@ -1,7 +1,7 @@
 require 'rails_helper'
 
  RSpec.describe 'merchant items api' do
-   it 'get all merchant items' do
+   it 'get all merchant items(happy path, fetch all items)' do
      merchant = create(:merchant)
      create_list(:item, 10, merchant_id: merchant.id)
 
@@ -12,5 +12,16 @@ require 'rails_helper'
      items = JSON.parse(response.body, symbolize_names: true)[:data]
 
      expect(items.count).to eq(10)
+   end
+
+   it 'sad path, bad integer id returns 404' do
+     get '/api/v1/merchants/8923987297/items'
+
+     expect(response).to_not be_successful
+     expect(response.status).to eq(404)
+
+     error = JSON.parse(response.body, symbolize_names: true)
+
+     expect(error).to have_key(:message)
    end
  end
