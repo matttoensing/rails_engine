0437033f9cf4586f5ea22628566f43f3a2975731
Matt Toensing <matthew.toensing@gmail.com>
2021-09-09 10:50:41 -0700

    Refactor conditionals to be in private methods

--- app/controllers/api/v1/revenue/merchants_controller.rb
+++ app/controllers/api/v1/revenue/merchants_controller.rb
@@ -1,22 +1,37 @@
 class Api::V1::Revenue::MerchantsController < ApplicationController
 
+  MERCHANT_COUNT = Merchant.count
+
   def index
-    if !params[:quantity].present?
-      json_response(MerchantRevenueSerializer.merchants_top_revenue_error, 400)
-    elsif params[:quantity].present? && params[:quantity].to_i == 0
-      json_response(MerchantRevenueSerializer.merchants_top_revenue_error, 400)
-    elsif params[:quantity].to_i > 100
-      merchant_count = Merchant.count
-      merchants = Merchant.merchants_with_most_revenue(merchant_count)
-      json_response(MerchantRevenueSerializer.format_top_revenue(merchants), 200)
+    if missing_quantity?
+      json_response(MerchantNameRevenueSerializer.merchants_top_revenue_error, :bad_request)
+    elsif quanity_is_a_string?
+      json_response(MerchantNameRevenueSerializer.merchants_top_revenue_error, :bad_request)
+    elsif quantity_greater_than_merchant_count?
+      merchants = Merchant.merchants_with_most_revenue(MERCHANT_COUNT)
+      json_response(MerchantNameRevenueSerializer.new(merchants))
     else
-      merchants = Merchant.merchants_with_most_revenue(params['quantity'])
-      json_response(MerchantRevenueSerializer.format_top_revenue(merchants), 200)
+      merchants = Merchant.merchants_with_most_revenue(params[:quantity])
+      json_response(MerchantNameRevenueSerializer.new(merchants))
     end
   end
 
   def show
     merchant = Merchant.find(params[:id])
     json_response(MerchantRevenueSerializer.new(merchant))
+  end
+
+  private
+
+  def missing_quantity?
+    !params[:quantity].present?
+  end
+
+  def quanity_is_a_string?
+    params[:quantity].present? && params[:quantity].to_i == 0
+  end
+
+  def quantity_greater_than_merchant_count?
+    params[:quantity].to_i >= MERCHANT_COUNT
   end
 end
