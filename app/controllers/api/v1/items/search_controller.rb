50cd8ea68975f03d4e8d175fd5b7213b74393835
Matt Toensing <matthew.toensing@gmail.com>
2021-09-09 10:30:30 -0700

    Refactor conditionals to be in private methods

--- app/controllers/api/v1/items/search_controller.rb
+++ app/controllers/api/v1/items/search_controller.rb
@@ -1,6 +1,9 @@
 class Api::V1::Items::SearchController < ApplicationController
+
+  MAX_MIN_PRICE = 10000
+
   def index
-    if !params[:max_price].present? && !params[:min_price].present?
+    if search_by_name_only?
       item = Item.search_results(params[:name])
       return json_response(ItemSerializer.items_error_message(params[:name]), 400) if item.nil?
 
@@ -9,31 +12,55 @@ class Api::V1::Items::SearchController < ApplicationController
 
     else
 
-      if params[:name].present? && params[:min_price].present?
-        json_response(ItemSerializer.item_min_price_search_error, 400)
+      if searching_for_name_and_price_range?
+        json_response(ItemSerializer.item_min_price_search_error, :bad_request)
 
-      elsif params[:name].present? && params[:max_price].present?
-        json_response(ItemSerializer.item_min_price_search_error, 400)
-
-      elsif params[:min_price].present? && params[:min_price].to_i > 10000
+      elsif min_price_out_of_bounds?
         json_response(ItemSerializer.item_min_price_too_big)
 
-      elsif params[:min_price].present? && params[:max_price].present?
+      elsif valid_min_and_max_price?
         item = Item.find_by_min_max_price(params[:min_price].to_i, params[:max_price].to_i)
+
         json_response(ItemSerializer.new(item))
 
-      elsif params[:min_price].present? && !params[:max_price].present?
-        return json_response(ItemSerializer.item_min_price_search_error, 400) if params[:min_price].to_i <= 0
+      elsif missing_max_price?
+        return json_response(ItemSerializer.item_min_price_search_error, :bad_request) if params[:min_price].to_i <= 0
 
         item = Item.find_by_min_price(params[:min_price].to_i)
         json_response(ItemSerializer.new(item))
 
-      elsif !params[:min_price].present? && params[:max_price].present?
-        return json_response(ItemSerializer.item_min_price_search_error, 400) if params[:max_price].to_i <= 0
+      elsif missing_min_price?
+        return json_response(ItemSerializer.item_min_price_search_error, :bad_request) if params[:max_price].to_i <= 0
 
         item = Item.find_by_max_price(params[:max_price].to_i)
         json_response(ItemSerializer.new(item))
       end
     end
+  end
+
+  private
+
+  def search_by_name_only?
+    !params[:max_price].present? && !params[:min_price].present?
+  end
+
+  def searching_for_name_and_price_range?
+    params[:name].present? && params[:min_price].present? || params[:name].present? && params[:max_price].present?
+  end
+
+  def min_price_out_of_bounds?
+    params[:min_price].present? && params[:min_price].to_i > MAX_MIN_PRICE
+  end
+
+  def valid_min_and_max_price?
+    params[:min_price].present? && params[:max_price].present?
+  end
+
+  def missing_max_price?
+    params[:min_price].present? && !params[:max_price].present?
+  end
+
+  def missing_min_price?
+    !params[:min_price].present? && params[:max_price].present?
   end
 end
