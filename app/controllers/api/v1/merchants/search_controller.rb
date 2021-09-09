3d27d5f7bc7af15b07ce3821ba1d75cc56550b81
Matt Toensing <matthew.toensing@gmail.com>
2021-09-09 10:33:04 -0700

    Include private method for missing name params, and change error response syntax

--- app/controllers/api/v1/merchants/search_controller.rb
+++ app/controllers/api/v1/merchants/search_controller.rb
@@ -1,12 +1,18 @@
 class Api::V1::Merchants::SearchController < ApplicationController
   def index
-    if params[:name] == ''
-      json_response(MerchantSerializer.merchant_id_string_error('name', params[:name]), 400)
+    if name_missing?
+      json_response(MerchantSerializer.merchant_id_string_error('name', params[:name]), :bad_request)
     else
       merchants = Merchant.search_results(params[:name])
-      return json_response(MerchantSerializer.merchants_error_message, 400) if merchants.nil?
+      return json_response(MerchantSerializer.merchants_error_message, :bad_request) if merchants.nil?
 
       json_response(MerchantSerializer.new(merchants))
     end
+  end
+
+  private
+
+  def name_missing?
+    params[:name] == ''
   end
 end
